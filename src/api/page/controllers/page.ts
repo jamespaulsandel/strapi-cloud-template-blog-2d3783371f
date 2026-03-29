01a3ee8f3c414fa36e37d2a6d295ec0971d6776b
Paul Sandel <paul.sandel@pm.me>
2026-03-29 14:09:59 -0400

    new controller for page.ts

--- src/api/page/controllers/page.ts
+++ src/api/page/controllers/page.ts
@@ -1,7 +1,38 @@
-/**
- * page controller
- */
-
+// src/api/page/controllers/page.ts
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::page.page');
+const PAGE_POPULATE = {
+  Body: {
+    on: {
+      'cms-content.hero': {
+        populate: { Availability: true, Image: true }
+      },
+      'cms-content.organization': {
+        populate: '*'
+      },
+      'cms-content.address': {
+        populate: '*'
+      },
+      'cms-content.list': {
+        populate: '*'
+      },
+      'cms-content.common-fields': {
+        populate: '*'
+      },
+      'cms-content.availability': {
+        populate: '*'
+      },
+    }
+  }
+};
+
+export default factories.createCoreController('api::page.page', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query.populate = PAGE_POPULATE;
+    return super.find(ctx);
+  },
+  async findOne(ctx) {
+    ctx.query.populate = PAGE_POPULATE;
+    return super.findOne(ctx);
+  },
+}));
